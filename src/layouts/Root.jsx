a822498416c5d0e3530b465f12694cecec07c335
alimran-dev <alimran5air@gmail.com>
2023-11-12 00:29:34 +0600

    footer added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar";
+import Footer from "../pages/Shared/Footer";
 
 const Root = () => {
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <div className="min-h-[70vh]">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
